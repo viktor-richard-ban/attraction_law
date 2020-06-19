eaecf8c46dc7eeb53698eb10e1ff1cacf3d94925
Molnár Imre <h770367@stud.u-szeged.hu>
2020-06-19 23:54:23 +0200

    Alap animációk hozzáadása

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,38 +1,62 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    private GameObject player;
+    private GameObject _player;
+    private Animator _animator;
     public float speed = 0.1f;
+
     void Start()
     {
-        player = GameObject.Find("Player");
+        _player = GameObject.Find("Player");
+        _animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        Vector3 playerPos = player.transform.position;
+        Vector3 playerPos = _player.transform.position;
 
+        _animator.enabled = false;
         if (Input.GetKey(KeyCode.W))
         {
+            SetParamToTrueAndOthersToFalse("isMoveAway");
             playerPos.y += speed;
         }
-
-        if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
+            SetParamToTrueAndOthersToFalse("isMoveForward");
             playerPos.y -= speed;
         }
-
-        if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
+            SetParamToTrueAndOthersToFalse("isMoveSideLeft");
             playerPos.x -= speed;
         }
-
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
+            SetParamToTrueAndOthersToFalse("isMoveSideRight");
             playerPos.x += speed;
         }
+        else
+        {
+            SetParamToTrueAndOthersToFalse("isIdle");
+        }
+        _animator.enabled = true;
+        _player.transform.position = playerPos;
+    }
 
-        player.transform.position = playerPos;
+    private void SetParamToTrueAndOthersToFalse(string name)
+    {
+        if (_animator.GetBool(name)) return;
+        _animator.SetBool(name, true);
+        foreach (AnimatorControllerParameter param in _animator.parameters)
+        {
+            if (param.name.Equals(name)) continue;
+            if (param.type == AnimatorControllerParameterType.Bool)
+            {
+                _animator.SetBool(param.name, false);
+            }
+        }
     }
 }
--- Assets/Scripts/StickyBehavior.cs
+++ Assets/Scripts/StickyBehavior.cs
@@ -1,24 +1,44 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class StickyBehavior : MonoBehaviour
 {
-    private GameObject stickyBody;
+    private GameObject _stickyBody;
+    private List<GameObject> _stickedObjects;
+    private Animator _animator;
+
     void Start()
     {
-        stickyBody = GameObject.Find("StickyBody");
-    }
-
-    void FixedUpdate()
-    {
-        
+        _stickyBody = GameObject.Find("StickyBody");
+        _stickedObjects = new List<GameObject> { };
+        _animator = GetComponent<Animator>();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("WoodThing"))
         {
-            Debug.Log("Collide");
-            other.transform.SetParent(stickyBody.transform);
-        } 
+            other.transform.SetParent(_stickyBody.transform);
+            _stickedObjects.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("ResetBush"))
+        {
+            RemoveStickedObjects();
+        }
+    }
+
+    private void RemoveStickedObjects()
+    {
+        Debug.Log(_stickedObjects.Count);
+        foreach (GameObject gameObject in _stickedObjects)
+        {
+            gameObject.transform.SetParent(null);
+        }
+
+        _stickedObjects.Clear();
     }
 }
