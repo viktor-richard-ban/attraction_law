0369ce11581294ed0f0ffd8c29c3044c44a068be
Molnár Imre <h770367@stud.u-szeged.hu>
2020-06-19 21:47:26 +0200

    Ragadás alap szinten hozzáadva

---
+++ Assets/Scripts/StickyBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class StickyBehavior : MonoBehaviour
+{
+    private GameObject stickyBody;
+    void Start()
+    {
+        stickyBody = GameObject.Find("StickyBody");
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("WoodThing"))
+        {
+            Debug.Log("Collide");
+            other.transform.SetParent(stickyBody.transform);
+        } 
+    }
+}
