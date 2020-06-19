19258f1b852200b338e4000a1bcc246972f56a50
Molnár Imre <h770367@stud.u-szeged.hu>
2020-06-19 20:55:59 +0200

    Mozgás hozzáadása

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private GameObject player;
+    public float speed = 0.1f;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 playerPos = player.transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerPos.y += speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerPos.y -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerPos.x -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerPos.x += speed;
+        }
+
+        player.transform.position = playerPos;
+    }
+}
