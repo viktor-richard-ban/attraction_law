3ea8a3fcf855b0b5da2f9b66e5113e4780a99702
Viktor Bán <h750676@stud.u-szeged.hu>
2020-06-21 00:33:10 +0200

    Colliders

--- Assets/Scripts/PressurePlateController.cs
+++ Assets/Scripts/PressurePlateController.cs
@@ -2,16 +2,19 @@
 
 public class PressurePlateController : MonoBehaviour
 {
-    public int sensitivity = 0;
+    public int sensitivity = 2;
     public bool isPressed = false;
+    public GameObject door;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Rabbit"))
             isPressed = true;
         if (other.gameObject.CompareTag("Player") &&
-            other.gameObject.GetComponent<CharacterMovement>().GetNumberOfStickedObjects() >= sensitivity)
+            other.gameObject.GetComponent<CharacterMovement>().GetNumberOfStickedObjects() >= sensitivity){
             isPressed = true;
+            door.gameObject.SetActive(false);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
