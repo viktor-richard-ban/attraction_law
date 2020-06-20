c4301c55bed183a2dfad5d57de2ae75df0029a6d
Molnár Imre <h770367@stud.u-szeged.hu>
2020-06-20 13:24:02 +0200

    .gitignore frissítése

---
+++ Assets/Scripts/LeverController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverController : MonoBehaviour
+{
+    public GameObject otherLever;
+    public bool isDisabled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isDisabled) {
+            this.gameObject.SetActive(false);
+            otherLever.SetActive(true);
+        }else {
+            this.gameObject.SetActive(true);
+            otherLever.SetActive(false);
+        }
+    }
+
+}
