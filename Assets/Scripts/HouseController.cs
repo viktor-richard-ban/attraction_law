9045dc9b4c41db22c59624a06ef59a984cf4c2ac
Molnár Imre <h770367@stud.u-szeged.hu>
2020-06-20 19:07:06 +0200

    House prefab befejezése

---
+++ Assets/Scripts/HouseController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
