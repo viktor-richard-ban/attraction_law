95f96f7ebc1fda486d7d14a6216b1959c36467ce
Viktor Bán <h750676@stud.u-szeged.hu>
2020-06-21 02:16:29 +0200

    Last level

---
+++ Assets/Scripts/FairyDoorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FairyDoorController : MonoBehaviour
+{
+
+    public GameObject leftTop;
+    public GameObject leftBottom;
+    public GameObject rightTop;
+    public GameObject rightBottom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(leftTop.GetComponent<PressureCollector>().items == 3 &&
+            leftBottom.GetComponent<PressureCollector>().items == 1 &&
+            rightTop.GetComponent<PressureCollector>().items == 2 &&
+            rightBottom.GetComponent<PressureCollector>().items == 3) { this.gameObject.SetActive(false); }
+            
+    }
+}
---
+++ Assets/Scripts/PressureCollector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressureCollector : MonoBehaviour
+{
+
+    public int items = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        items+=1;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        items-=1;
+    }
+
+}
