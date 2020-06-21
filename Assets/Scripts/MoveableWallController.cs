57b7ff82a8a1d11a63fe8686249ed8ce85320fc6
Molnár Imre <h770367@stud.u-szeged.hu>
2020-06-21 17:38:53 +0200

    Hidden Room hozzáadva

---
+++ Assets/Scripts/MoveableWallController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MoveableWallController : MonoBehaviour
+{
+    public GameObject hider;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            hider.SetActive(false);
+        }
+    }
+}
