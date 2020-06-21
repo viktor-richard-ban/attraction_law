5e0a49fabea75d3644949de07fdd8c8b9de99a4e
Molnár Imre <h770367@stud.u-szeged.hu>
2020-06-21 19:04:02 +0200

    Demo end title hozzáadva

---
+++ Assets/Scripts/DemoEnd.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DemoEnd : MonoBehaviour
+{
+    public GameObject demoEnd;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            demoEnd.SetActive(true);
+        }
+    }
+}
